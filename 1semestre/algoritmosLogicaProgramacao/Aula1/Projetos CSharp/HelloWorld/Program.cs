3af92b039461931d14778ef206f11a13ce9401d7
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-02-21 10:25:09 -0400

    up1

--- 1semestre/algoritmosLogicaProgramacao/Aula1/Projetos CSharp/HelloWorld/Program.cs
+++ 1semestre/algoritmosLogicaProgramacao/Aula1/Projetos CSharp/HelloWorld/Program.cs
@@ -1,8 +1,40 @@
-﻿
-Console.WriteLine( " " );                                                               
- Console.WriteLine("   _|      _|              _|      _|                                      ");
- Console.WriteLine("  _|_|  _|_|    _|_|_|  _|_|_|_|  _|_|_|      _|_|    _|    _|    _|_|_|  ");
- Console.WriteLine("  _|  _|  _|  _|    _|    _|      _|    _|  _|_|_|_|  _|    _|  _|_|      ");
- Console.WriteLine("  _|      _|  _|    _|    _|      _|    _|  _|        _|    _|      _|_|  ");
- Console.WriteLine("  _|      _|    _|_|_|      _|_|  _|    _|    _|_|_|    _|_|_|  _|_|_|    ");
+﻿/*int valor1;
+int valor2;
+
+int resultado;
+
+
+valor1 = 10;
+valor2 = 20;
+resultado = valor1 * valor2;
+Console.WriteLine(resultado);
+*/
+
+/*//Estrutura condicional  (IF/ElSE)
+
+int idadeDoGustavo;
+Console.WriteLine("Digite a idade do Gustavo: ");
+//A idade do Gustavo vai receber o que o usuário digitar
+idadeDoGustavo = int.Parse(Console.ReadLine());
+
+if(idadeDoGustavo >= 18){
+    Console.WriteLine("Pode tirar a carteira de motorista");
+}else{
+    Console.WriteLine("O Gustavo não tem idade suficiente!");
+}
+
+Console.ReadKey();
+*/
+// Atividade valor da mesada para ir no rolê
+
+int mesada;
+Console.WriteLine("Digite o valor da sua mesada: ");
+mesada = int.Parse(Console.ReadLine());
+
+if(mesada >= 200) {
+    Console.WriteLine("Você tem grana pra balada :)");
+} else{
+    Console.WriteLine("Você só tem dinheiro pro cinema :/");
+}
+
 Console.ReadKey();
