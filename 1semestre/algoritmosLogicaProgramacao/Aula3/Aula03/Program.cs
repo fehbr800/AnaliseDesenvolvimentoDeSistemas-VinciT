14b72d88eafb86c8a13630287e5daf5898ee79bf
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-03-07 14:35:46 -0400

    up1

---
+++ 1semestre/algoritmosLogicaProgramacao/Aula3/Aula03/Program.cs
@@ -0,0 +1,21 @@
+﻿//inteiro
+//decimal
+//letra
+//palavra
+
+// Laços de repetição
+// FOR
+// WHILE
+// DO ... WHILE
+// LAÇO ESPECIAL FOREACH (POO)
+
+// TODO LAÇO DE REPETIÇÃO TEM: 
+//INICIO E CONDIÇAO DE PARADA
+
+int contador;
+
+for(contador = 0; contador < 10; contador++){
+    Console.WriteLine("Matheus");
+}
+
+Console.ReadKey();
